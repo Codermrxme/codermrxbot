d1340d23ea90bda5b8c92857cf9b0f74b5b36267
Codermrx <codermrxme.@gmail.com>
2025-10-22 04:25:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -438,7 +438,7 @@ class HealthHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'OK')
+            self.wfile.write(b'@codermrxbot ishlayapti 🚀')
         else:
             self.send_response(404)
             self.end_headers()
